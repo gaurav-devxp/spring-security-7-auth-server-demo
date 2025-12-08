6077a180f1bdd144d8c6cea65bc94a7271811b4c
Gaurav Pandey <gauravdev.xp@gmail.com>
2025-12-08 19:04:08 +0530

    Initial Commit

---
+++ src/main/java/dev/gaurav/springauthserverdemo/SpringAuthServerDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.gaurav.springauthserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gaurav/springauthserverdemo/SpringAuthServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gaurav.springauthserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
